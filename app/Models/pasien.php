17675cfafcaae492a603d3be0c255fd08f109ef8
TasyaW12 <tasyawangge1205@gmail.com>
2025-09-24 09:18:42 +0700

    Membuat kelas model

---
+++ app/Models/pasien.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class pasien extends Model
+{
+    use HasFactory;
+
+    protected $table = 'pasien';
+    protected $primaryKey = 'id_pasien';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'id_pasien',
+        'nama',
+        'umur',
+        'alamat',
+        'no_telp',
+        'tipe_pasien'
+    ];
+    public $timestamps = false;
+
+    // Relasi ke pendaftaran
+    public function pendaftarans()
+    {
+        return $this->hasMany(Pendaftaran::class, 'id_pasien', 'id_pasien');
+    }
+
+}
