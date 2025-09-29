c2c51d0d711488850f0966be45121c11f8a21a00
angelytacell <angelytacelltya@gmail.com>
2025-09-29 14:34:49 +0700

    Membuat kelas model jadwal dokter

---
+++ app/Models/JadwalDokter.php
@@ -0,0 +1,24 @@
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class JadwalDokter extends Model
+{
+    use HasFactory;
+
+    protected $table = 'jadwal_dokter';
+    protected $primaryKey = 'id_jadwal';
+    public $timestamps = false;   // tabel tidak punya created_at/updated_at
+
+    protected $fillable = [
+        'id_dokter',
+        'jadwal_dokter'
+    ];
+
+    // Relasi ke Dokter
+    public function dokter()
+    {
+        return $this->belongsTo(Dokter::class, 'id_dokter', 'id_dokter');
+    }
+}
