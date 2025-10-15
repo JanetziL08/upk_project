f715c865e66ddcc4e2292e7222f51e689188b2a1
tamurambu <tamurambutd@gmail.com>
2025-10-15 16:40:39 +0700

    Buat View

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\rekamMedisController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::match(['get', 'post'], '/rekam-medis/{id_pasien}/terapi', [rekamMedisController::class, 'inputTerapi'])
+    ->name('rekam-medis.input-terapi');
+
+Route::get('/rekam-medis/tes-terapi', function () {
+    return view('input_terapi');
 });
